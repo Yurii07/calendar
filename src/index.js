7a4243aca71724963957b077c1b8276bf04f6627
Yurii Adamenko <skyey07@gmail.com>
2019-06-20 11:38:37 +0300

    include Calendar component to index

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello');
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Calendar from "./components/calendar";
+
+function App (){
+    return(
+        <div className="App">
+            <Calendar />
+        </div>
+    );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
